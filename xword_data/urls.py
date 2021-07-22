d8e90b70e528c7569996a028943c26192448a35e
ehigie aito (Pystar) <aitoehigie@gmail.com>
2021-07-22 05:35:36 +0100

    feat: add a random data selector function and HTTP function
    Added a random data selector helper function to fetch random clues from the Clue table and also a drill method to handle HTTP requests to the /drill URL.

---
+++ xword_data/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('drill/', views.drill, name='drill'),
+    path('answer/', views.answer, name='answer'),
+]
--- xword_data/views.py
+++ xword_data/views.py
@@ -1,3 +1,21 @@
+from random import randint
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+from .models import Puzzle, Clue, Entry
+
+
+def random_selector():
+    maximum_id = Clue.objects.last().id
+    random_id = randint(0, maximum_id)
+    return Clue.objects.get(id=random_id)
+    
+
+def drill(request):
+    random_clue = random_selector()
+    clue_length = len(random_clue.entry.entry_text)
+    clue_puzzle = random_clue.puzzle.title
+    return render(request, 'xword_data/drill.html', {'clue_length': clue_length, 'clue_puzzle': clue_puzzle})
+
+def answer(request):
+    return HttpResponse('Hello from answer')
